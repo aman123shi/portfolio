6b3d8c9d9fa300b5126e58beffc6610df1497995
Amanuel Shiferaw <123aman64@gmail.com>
2022-10-23 13:49:06 +0300

    Add Social media links in constant

--- src/app/pages/intro/intro.component.ts
+++ src/app/pages/intro/intro.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Socials } from "../../constants/social";
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
-
+ socials = Socials;
   constructor() { }
 
   ngOnInit(): void {
